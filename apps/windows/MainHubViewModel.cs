1ce7b17b709009ec7b15dba8393a02760efac68c
Buipui <buipui@outlook.com>
2026-09-19 08:19:41 +0200

    Connect render pipeline to Main Hub clients

--- apps/windows/MainHubViewModel.cs
+++ apps/windows/MainHubViewModel.cs
@@ -1,13 +1,10 @@
 namespace Biupiu.Desktop;
-
 public sealed class MainHubViewModel
 {
-    public IReadOnlyList<RuntimeRouteState> Destinations { get; }
-
-    public MainHubViewModel(RuntimeSession session)
-    {
-        Destinations = new[] { "SMART_FARMING", "SMART_METAL_WORKSHOP", "RND_OS" }
-            .Select(route => BiupiuRuntime.ResolveRoute(session, route))
-            .ToList();
-    }
+ public IReadOnlyList<RuntimeRouteState> Destinations { get; }
+ public MainHubViewModel(RuntimeSession session)
+ {
+  Destinations=new[]{"SMART_FARMING","SMART_METAL_WORKSHOP","RND_OS","RENDER_PIPELINE"}
+   .Select(route=>BiupiuRuntime.ResolveRoute(session,route)).ToList();
+ }
 }
