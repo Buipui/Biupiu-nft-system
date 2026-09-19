47ede9f7d8e6778aa28685bc96a0d8daa553063d
Buipui <buipui@outlook.com>
2026-09-19 10:09:01 +0200

    feat(os): export digital twin and DMS adapters

--- packages/biupiu-rnd-os/src/index.ts
+++ packages/biupiu-rnd-os/src/index.ts
@@ -1,1 +1,8 @@
-export const BIUPIU_APP="Biupiu R&D OS"; export const PACKAGE="@biupiu/rnd-os";
+export const BIUPIU_APP="Biupiu R&D OS";
+export const PACKAGE="@biupiu/rnd-os";
+
+export * from "./platform";
+export * from "./java";
+export * from "./linux";
+export * from "./digital-twin";
+export * from "./dms";
